b353ce7133a409eabc4b622e22d259c15b65563a
TheArchitect4855 <kurtis@kurtisknodel.com>
2022-11-14 17:29:20 -0800

    Added 404 page

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
     import Footer from './components/Footer.svelte';
     import Header from './components/Header.svelte';
 	import Home from './pages/Home.svelte';
+    import NotFound from './pages/NotFound.svelte';
 
 	const routes = [
 		{ path: '/', component: Home },
+		{ path: '/.+', component: NotFound },
 	];
 </script>
 
---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { link } from "svelte-spa-history-router";
+
+</script>
+<main class="container">
+	<h1>There's nothing here.</h1>
+	<h3>Perhaps you got lost...</h3>
+	<a href="/" use:link>Take me Home</a>
+</main>
+
+<style>
+	main {
+		margin-top: 20vh;
+		text-align: center;
+	}
+</style>
